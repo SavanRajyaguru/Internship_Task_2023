7349f628819c160119aa6757862f6548dcd55394
SavanRajyaguru <savanrajyaguru2001@gmail.com>
2023-02-07 12:48:09 +0530

    implement basic animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:project1/screens/feb2_screen/splash_screen_2Feb.dart';
 import 'package:project1/screens/feb3_screen/phone_directory_screen_feb3.dart';
 import 'package:project1/screens/feb6_screen/custom_widget_demo.dart';
+import 'package:project1/screens/feb7_screen/animation_demo.dart';
 
 
 void main() {
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CustomWidgetDemo(),
+      home: AnimationDemo(),
     );
   }
 }
